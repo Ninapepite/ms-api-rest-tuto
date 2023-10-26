3ee35d66438dce8544f42164ca54eaf70a3f085b
Ninapepite <killian.stein@icloud.com>
2023-10-26 09:34:18 +0200

    OpenAI Route

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 // Importation de la librairie express
 const express = require('express');
+// Importation de la librairie openai
+import OpenAI from "openai";
+// Creation d'une instance de l'api openai
+const openai = new OpenAI();
+// Async méthode pour générer une image avec OpenAI
+async function generateImage() {
+    // Génération d'une image avec OpenAI
+    const image = await openai.images.generate({ prompt: "A cute cat please" });
+    // Affichage de l'image générée
+    console.log(image.data);
+}
+
 // Creation de l'instance express
 const app = express();
 // Port d'écoute de l'application
 const port = 3000;
 // Creation d'une route par défaut
 app.get('/api', (req, res) => res.send("Hello World ! L'api fonctionne bien"));
+app.get('/api/ai', (req, res) => { generateImage(); });
 // Lancement de l'application
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
