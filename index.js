eb1cba054906991e5869b10bae3e03a36c6b473e
Ninapepite <killian.stein@icloud.com>
2023-10-26 09:51:37 +0200

    add fs

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 // Importation de la librairie express
 const express = require('express');
 // Importation de la librairie openai
-import OpenAI from "openai";
+const OpenAI = require('openai');
+//Import fs path librairie
+const fs = require('fs');
 // Creation d'une instance de l'api openai
 const openai = new OpenAI();
 // Async méthode pour générer une image avec OpenAI
 async function generateImage() {
     // Génération d'une image avec OpenAI
     const image = await openai.images.generate({ prompt: "A cute cat please" });
-    // Affichage de l'image générée
+    // Affichage de l'image générée dans le terminal
     console.log(image.data);
+    // Enregistrer l'image dans un dossier local /imagesIA
+    fs.writeFile("./imagesIA/image.png", image.data, 'binary', function (err) {
+        if (err) throw err;
+        console.log('Image saved.');
+    });
+
 }
 
 // Creation de l'instance express
@@ -18,6 +26,7 @@ const app = express();
 const port = 3000;
 // Creation d'une route par défaut
 app.get('/api', (req, res) => res.send("Hello World ! L'api fonctionne bien"));
+//Route pour générer l'image
 app.get('/api/ai', (req, res) => { generateImage(); });
 // Lancement de l'application
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
